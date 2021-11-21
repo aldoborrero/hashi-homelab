298f2693c4494301f5a76ecb146819bce40e9677
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2021-11-21 17:21:04 +0100

    fix: solved minor mistakes in nuc.nix

--- ops/nixos/nuc-1/users/nuc.nix
+++ ops/nixos/nuc-1/users/nuc.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
 in
 {
   imports = [
@@ -125,5 +125,5 @@ in
         ];
       };
     };
-  }
-    }
+  };
+}
