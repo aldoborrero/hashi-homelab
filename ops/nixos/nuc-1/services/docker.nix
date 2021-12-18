9b6df4cc4c8fd5715ac3d72675850a98403471f3
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2021-12-18 21:54:46 +0100

    feat: more changes

--- ops/nixos/nuc-1/services/docker.nix
+++ ops/nixos/nuc-1/services/docker.nix
@@ -4,5 +4,6 @@
   virtualisation.docker = {
     enable = true;
     autoPrune.enable = true;
+    liveRestore = false; # Allow dockerd to be restarted without affecting running container. This option is incompatible with docker swarm.
   };
 }
